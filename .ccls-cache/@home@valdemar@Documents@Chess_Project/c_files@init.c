10dd0a12096fcd3ede3db90d7c09c62654661217
helios1014 <helios1014@gmail.com>
2025-08-03 19:51:32 -0400

    fixing the segmentation fault

--- .ccls-cache/@home@valdemar@Documents@Chess_Project/c_files@init.c
+++ .ccls-cache/@home@valdemar@Documents@Chess_Project/c_files@init.c
@@ -2,15 +2,15 @@
 #include "defs.h"
 
 
-int Sq120ToSq64[BRD_SQ_NUM] ;
-int Sq64ToSq120[64] ;
+int Sq120ToSq64[BRD_SQ_NUM];
+int Sq64ToSq120[64];
 
-void InitSq120ToSq64 () {
+void InitSq120ToSq64() {
 
     int index = 0;
     int file = FILE_A;
     int rank = RANK_1;
-    int sq =A1;
+    int sq = A1;
     int sq64 = 0;
     for(index = 0; index < BRD_SQ_NUM; ++index) {
       Sq120ToSq64[index] = 65; //sets all values to number that is not on the board
@@ -22,15 +22,15 @@ void InitSq120ToSq64 () {
 
     for(rank = RANK_1; rank <= RANK_8; ++index) {
         for(file = FILE_A; file <= FILE_H; ++file) {
-        sq = FR2SQ (file,rank);
+        sq = FR2SQ(file,rank);
         Sq64ToSq120[sq64] = sq;
         Sq120ToSq64[sq] = sq64;
-        sq64++ ;
+        sq64++;
       }
     }
   }
 
-void AllInit () {
-  InitSq120ToSq64 () ;
+void AllInit() {
+  InitSq120ToSq64();
 
 }
--- c_files/init.c
+++ c_files/init.c
@@ -2,15 +2,15 @@
 #include "defs.h"
 
 
-int Sq120ToSq64[BRD_SQ_NUM] ;
-int Sq64ToSq120[64] ;
+int Sq120ToSq64[BRD_SQ_NUM];
+int Sq64ToSq120[64];
 
-void InitSq120ToSq64 () {
+void InitSq120ToSq64() {
 
     int index = 0;
     int file = FILE_A;
     int rank = RANK_1;
-    int sq =A1;
+    int sq = A1;
     int sq64 = 0;
     for(index = 0; index < BRD_SQ_NUM; ++index) {
       Sq120ToSq64[index] = 65; //sets all values to number that is not on the board
@@ -20,17 +20,17 @@ void InitSq120ToSq64 () {
       Sq64ToSq120[index] = 120; //sets all values to number that is not on the board
     }
 
-    for(rank = RANK_1; rank <= RANK_8; ++index) {
+    for(rank = RANK_1; rank <= RANK_8; ++rank) {
         for(file = FILE_A; file <= FILE_H; ++file) {
-        sq = FR2SQ (file,rank);
+        sq = FR2SQ(file,rank);
         Sq64ToSq120[sq64] = sq;
         Sq120ToSq64[sq] = sq64;
-        sq64++ ;
+        sq64++;
       }
     }
   }
 
-void AllInit () {
-  InitSq120ToSq64 () ;
+void AllInit() {
+  InitSq120ToSq64();
 
 }
